d185d34fafccea7428347ca0694092acd463fa95
Ye Jianquan <ye_jianquan@126.com>
2018-05-07 22:49:49 +0800

    Initial commit

---
+++ ipv4_convertor.py
@@ -0,0 +1,38 @@
+def convert_ipv4(sAddress):
+    iResult = 0
+    str = ''
+    i = 1
+    bLastIsSpace = False
+    bAfterDot = False
+    setNumbers = set(['0','1','2','3','4','5','6','7','8','9'])
+    for s in sAddress:
+        if s == ' ':
+            bLastIsSpace = True
+        elif (not bAfterDot) and bLastIsSpace and (s != ' ' and s != '.'):
+            raise NameError('Error spaces')
+        elif bAfterDot and s == ' ':
+            bAfterDot = True
+        elif s == '.':
+            bLastIsSpace = False
+            bAfterDot = True
+            if str == '':
+                raise NameError('Each address number should not be null')
+            if len(str) > 3:
+                raise NameError('Each address number should not be 3-digits')
+            iStr = int(str)
+            if iStr > 255 :
+                raise NameError('Each address number should in [0,255]')
+            iResult = iResult << 8 ^ iStr
+            i += 1
+            str = ''
+        elif s not in setNumbers:
+            raise NameError('Each number should in [0-9]')
+        else :
+            bLastIsSpace = False
+            bAfterDot = False
+            str += s
+    iResult = iResult << 8 ^ int(str)
+    if i != 4:
+        raise NameError('Invalid format of string, please make sure 4 address numbers are given')
+    return iResult
+
---
+++ testcases.py
@@ -0,0 +1,66 @@
+import unittest
+from ipv4_convertor import convert_ipv4
+
+
+class convertorTest(unittest.TestCase):
+    def test_success_1(self):
+        self.assertEqual(convert_ipv4('172.168.5.1'), 2896692481, 'test fail')
+
+    def test_success_2(self):
+        self.assertEqual(convert_ipv4('192.168.1.1'), 3232235777, 'test fail')
+
+    def test_success_3(self):
+        self.assertEqual(convert_ipv4('0.0.0.0'), 0, 'test fail')
+
+    def test_success_3(self):
+        self.assertEqual(convert_ipv4('255.255.255.255'), 4294967295, 'test fail')
+
+    def test_success_space_1(self):
+        self.assertEqual(convert_ipv4('172. 168.5.1'), 2896692481, 'test fail')
+
+    def test_success_space_2(self):
+        self.assertEqual(convert_ipv4('172.168 . 5.1'), 2896692481, 'test fail')
+
+    def test_success_space_3(self):
+        self.assertEqual(convert_ipv4('172.     168.5.1'), 2896692481, 'test fail')
+
+    def test_success_space_4(self):
+        self.assertEqual(convert_ipv4('172      .168.5.1'), 2896692481, 'test fail')
+
+    def test_success_space_5(self):
+        self.assertEqual(convert_ipv4('172      .          168.5.1'), 2896692481, 'test fail')
+
+    def test_exception_wrong_spaces_1(self):
+        self.assertRaisesRegexp(NameError, 'Error spaces', convert_ipv4, '17 2.168.5.1')
+
+    def test_exception_wrong_spaces_2(self):
+        self.assertRaisesRegexp(NameError, 'Error spaces', convert_ipv4, '17  2.168.5.1')
+
+    def test_exception_null_address_number(self):
+        self.assertRaisesRegexp(NameError, 'Each address number should not be null', convert_ipv4, '172..5.1')
+
+    def test_exception_wrong_char_1(self):
+        self.assertRaisesRegexp(NameError, 'Each number should in \[0-9\]', convert_ipv4, 'a172.168.5.1')
+
+    def test_exception_wrong_char_2(self):
+        self.assertRaisesRegexp(NameError, 'Each number should in \[0-9\]', convert_ipv4, '-172.168.5.1')
+
+    def test_exception_wrong_char_3(self):
+        self.assertRaisesRegexp(NameError, 'Each number should in \[0-9\]', convert_ipv4, '-0.168.5.1')
+
+    def test_exception_wrong_char_4(self):
+        self.assertRaisesRegexp(NameError, 'Each address number should not be 3-digits', convert_ipv4, '0000.168.5.1')
+
+    def test_exception_wrong_char_5(self):
+        self.assertRaisesRegexp(NameError, 'Each address number should in \[0,255\]', convert_ipv4, '256.168.5.1')
+
+    def test_exception_ne_4_numbers_1(self):
+        self.assertRaisesRegexp(NameError, 'Invalid format of string, please make sure 4 address numbers are given',
+                                convert_ipv4, '192.168.5.1.1')
+
+    def test_exception_ne_4_numbers_2(self):
+        self.assertRaisesRegexp(NameError, 'Invalid format of string, please make sure 4 address numbers are given',
+                                convert_ipv4, '192.168.5')
+
+if __name__ == '__main__':
+    unittest.main()
